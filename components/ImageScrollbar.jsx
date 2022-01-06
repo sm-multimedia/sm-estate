5c1cc6d622670bc077938a327d9155be8d51862c
sm-multimedia <62784699+sm-multimedia@users.noreply.github.com>
2022-01-06 19:37:28 +0800

    Update ImageScrollbar.jsx
    key updated

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -40,7 +40,7 @@ const LeftArrow = () => {
     return (
       <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
         {data.map((item) => (
-          <Box width='910px' key={item.id} overflow='hidden' p='1'>
+          <Box width='910px' key={item.id} itemId={item.id} overflow='hidden' p='1'>
             <Image placeholder="blur" blurDataURL={item.url} alt="image" src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
           </Box>
         ))}
